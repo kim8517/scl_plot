b73edc794d5186b21a6fa65a80c55ef204c19c85
kim8517 <gimtaewan@MacBook-Pro-2.local>
2024-08-26 11:06:37 +0900

    first_commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# matplot setting for SNUCLUES
+To install run the following command:
+'''python setup.py'''
+This wll make your plot more visible and beautiful!
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scl_plot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['matplotlib'],
+    entry_points={
+        'console_scripts': [
+            # 'script_name = my_package.myscript:main_function',
+        ],
+    },
+    include_package_data=True,
+    description='Python matplotlib settings for SNUCLUES',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='TAEWAN KIM',
+    author_email='ktwa8517@gmail.com',
+    url='https://github.com/yourusername/my_package',  # Update with your package URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.',
+)
