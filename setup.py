3e52b84afdf8e11446c1a3e18a0615c6058b0361
kim8517 <96759588+kim8517@users.noreply.github.com>
2025-08-05 14:35:32 +0900

    Update setup.py (20250805)

--- setup.py
+++ setup.py
@@ -21,6 +21,5 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.',
+    ]
 )
